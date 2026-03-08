ebcaf8541014c7d7b3ecc2eb1613609da73f2ec0
bahati <bahatifidelis2004@gmail.com>
2026-03-08 11:48:09 +0300

    add functionality to read specific characters from a file using seek() and read()

---
+++ readcharacter.py
@@ -0,0 +1,12 @@
+file = open('file.txt', 'r')
+
+#If you want to read characters from a specific position in the file, you can use the seek() method. This method moves the file pointer (like a cursor) to a particular position.
+
+file.seek(10)  # Move to the 11th byte (0-based index)
+
+#To read specific characters, you can use the read() method with an argument that specifies the number of characters to read
+
+characters = file.read(5)  # Read 5 characters from the current position
+print(characters)
+
+file.close()
